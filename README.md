abe3130a783ba5c79d277c0e3763ece9121ea8e2
Bien Managbanag <bien.managbanag2@mail.dcu.ie>
2024-09-26 01:01:29 +0100

    Picked a dataset to work with, started to clean the dataset

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DWDM
 Data Warehousing &amp; Data Mining Project
+
+Using a Tourism Dataset obtained from Kaggle we are trying to predict which destinations or attractions will be most popular based on past visit patterns, traveler reviews, or marketing campaigns.
+
+Link to dataset: https://www.kaggle.com/datasets/umeradnaan/tourism-dataset
--- clean.py
+++ clean.py
@@ -3,23 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-td = pd.read_csv('tourism_dataset.csv')
-#tourism_data = tourism_data[['Country', 'Category', 'Visitors', 'Rating', 'Revenue', 'Accomadation_Available']]
+df = pd.read_csv('Property_Price_Register_Ireland.csv') # reads the csv
 
-remove = ['Location']
-td.drop(columns=remove, inplace=True)
+columns = df.columns # Columns of the dataset
 
-#plt.hist(td['Country'], bins=30, edgecolor='black')
-#plt.hist(td['Category'], bins=30, edgecolor='black')
-#plt.hist(td['Accommodation_Available'], bins=30, edgecolor='black')
+unfiltered_data = df.copy() #Copy of the original csv before changes
+
+df.drop('ADDRESS', axis=1, inplace=True) # Get rid of unnecessary columns
+sale_price_mean = df['SALE_PRICE'].mean() #Sale Price Mean = 259040.30628275065 = 259040.31
+print(sale_price_mean)
+
+
+#plt.hist(td['Country'], bins=30, edgecolor='black') # Histogram showing the countries most travelled to
 #plt.show()
 
-isnull = td.isna().sum() #Checking for null values
+#isnull = ds.isna().sum() #Checking for null values
 #print(isnull)
-max_visitors = max(td['Visitors'])
-max_rating = max(td['Rating'])
-max_revenue = max(td['Revenue'])
+#max_visitors = max(td['Visitors'])
 
-print(max_visitors)
-print(max_rating)
-print(max_revenue)
+#print(max_visitors)
+#print(max_rating)
+#print(max_revenue)
